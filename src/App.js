ee29dcd847db8492435a057076a0dc6971b6177a
testler <joshgarst@me.com>
2022-12-22 00:00:15 -0600

    loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <mesh>
+        <OrbitControls />
+        <mesh rotation-x={1}>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial />
         </mesh>
